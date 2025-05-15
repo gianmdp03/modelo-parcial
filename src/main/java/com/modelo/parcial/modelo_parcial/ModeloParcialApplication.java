1f1356bccc66e6e422a3f97336ad937d30b7a3f1
gianmdp03 <gcastorina234@gmail.com>
2025-05-15 14:07:44 -0300

    primer commit

---
+++ src/main/java/com/modelo/parcial/modelo_parcial/ModeloParcialApplication.java
@@ -0,0 +1,13 @@
+package com.modelo.parcial.modelo_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modelo/parcial/modelo_parcial/ModeloParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelo.parcial.modelo_parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
