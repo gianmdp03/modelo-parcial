75756a1ca0cc051cad6325edec13be124afa23d1
gianmdp03 <gcastorina234@gmail.com>
2025-05-21 21:35:19 -0300

    terminado + comentario many to many

--- src/main/java/com/modelo/parcial/modelo_parcial/ModeloParcialApplication.java
+++ src/main/java/com/modelo/parcial/modelo_parcial/ModeloParcialApplication.java
@@ -1,7 +1,11 @@
 package com.modelo.parcial.modelo_parcial;
 
+import jakarta.persistence.ManyToMany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class ModeloParcialApplication {
@@ -9,5 +13,14 @@ public class ModeloParcialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloParcialApplication.class, args);
 	}
+	/*@ManyToMany (lado peliculas)
+    @JoinTable(
+            name = "pelicula_actor",
+            joinColumns = @JoinColumn(name = "pelicula_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private Set<Actor> actores = new HashSet<>();*/
 
+	/*@ManyToMany(mappedBy = "actores") (lado actores)
+	private Set<Pelicula> peliculas = new HashSet<>();*/
 }
